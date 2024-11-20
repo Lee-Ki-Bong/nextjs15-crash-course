e0cd1de602ec9b7adea739df09176ddf60bcb175
dhkehd2 <dhkehd2@gmail.com>
2024-11-20 11:27:42 +0900

    📝 서버사이드 컴포넌트에서의 console.log

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
 export default function Home() {
+  console.log('from Server');
   return (
     <div className="flex items-center justify-center h-screen">
       <h1 className="text-3xl">Welcome to Next.js</h1>
