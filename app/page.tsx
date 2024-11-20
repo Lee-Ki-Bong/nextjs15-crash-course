04b3ee0be7fea8da516d8ebdd01013127885bf66
dhkehd2 <dhkehd2@gmail.com>
2024-11-20 14:23:52 +0900

    🔨 라우트 그룹별 레이아웃 추가해보기

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 import Hello from "./components/hello";
 
 export default function Home() {
-  console.log('from Server');
+  console.log("from Server");
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-3xl">Welcome to Next.js</h1>
       <Hello />
     </div>
