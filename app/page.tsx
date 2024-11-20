931f76948501a5ee450f93baaaa901b505d0800b
dhkehd2 <dhkehd2@gmail.com>
2024-11-20 11:39:08 +0900

    📝 클라이언트사이드 컴포넌트에서의 console.log

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
+import Hello from "./components/hello";
 
 export default function Home() {
   console.log('from Server');
   return (
     <div className="flex items-center justify-center h-screen">
       <h1 className="text-3xl">Welcome to Next.js</h1>
+      <Hello />
     </div>
   );
 }
